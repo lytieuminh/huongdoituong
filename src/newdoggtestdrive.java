9648e08c93c66194a25d02451955aa9661035f12
Bangly <lytieubang.93@gmail.com>
2022-02-24 10:37:37 +0700

    reset

--- src/newdoggtestdrive.java
+++
@@ -1,7 +0,0 @@
-public class newdoggtestdrive {
-    public static void main(String[] args) {
-        newdogg dog = new newdogg();
-        dog.setSize(4);
-        System.out.println(" Dog Size " + dog.setSize());
-    }
-}
